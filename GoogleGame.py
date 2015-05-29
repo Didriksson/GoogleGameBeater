ed400908442f01510bc6c4f300275563358663af
Didriksson <mattias.didriksson@gmail.com>
2015-05-29 16:30:11 +0200

    Actual file aswell...

--- GoogleGame.py
+++ GoogleGame.py
@@ -1,31 +1,66 @@
 from PIL import ImageGrab
-import time
-import winsound
-import ImageOps
+import time    
+import winsound, cv2
 import numpy as np
 import win32api, win32con
 
 
-screen = (510, 205, 580, 245)
+gameWidth = 600
+gameHeigth = 165
+
+
+#Set up variables such as resolution of gamescreen etc.
+def setUp():
+	global gamescreen
+	global checkForObstacleUpperLeft
+	image = grabImage()
+	posX = image.size[0]/2 - 300
+	posY = 125
+	gamescreen = (posX,posY, posX + gameWidth, posY + gameHeigth)
+	checkForObstacleUpperLeft = (gameWidth/100 * 30, gameHeigth/100 * 70)
+
+#Draws the box which is used to detect a tree.	
+def drawRectangle(img):
+	global checkForObstacleUpperLeft
+	img = np.array(img)
+	img = cv2.rectangle(img,checkForObstacleUpperLeft, (checkForObstacleUpperLeft[0] + 50, checkForObstacleUpperLeft[1] + 50),(255,0,0),1)
+	return img
+	"""
+	#checkForObstacleUpperLeft[0]:checkForObstacleUpperLeft[0]+50, checkForObstacleUpperLeft[1]:checkForObstacleUpperLeft[1]+50
+	print checkForObstacleUpperLeft[1]
+	box = img[70:120, 180:230]
+	print box
+	cv2.imwrite('box.jpeg', box)
+	"""
+#Saves using OpenCV
+def saveImage(img, path):
+	cv2.imwrite(path,img)
 
 #Grabs image with the size and position of the specified box.
-def grabImage(box):
+def grabImage(box = None):
 	im = ImageGrab.grab(box)
-	return im	
+	return im
+#Pushes space and waits 0.1 and then send command for releasing space.	
+def jump():
+	win32api.keybd_event(win32con.VK_SPACE, 0, win32con.KEYEVENTF_EXTENDEDKEY | 0, 0)
+	time.sleep(0.1)
+	win32api.keybd_event(win32con.VK_SPACE, 0, win32con.KEYEVENTF_EXTENDEDKEY | win32con.KEYEVENTF_KEYUP, 0)
 
-	
+setUp()	
 time.sleep(2)
 bild = 1
+grabImage(gamescreen).save('helabilden.jpeg', 'JPEG')
 
 #loop that keeps on going 'til end of program. Will grab the image and convert to a set. If the set is not all white,
 #some obstacle is in the way - i.e. JUMP!
 while True:
-	image = grabImage(screen)
-	setOFPixels = set(image.convert('L').getdata())
-	if len(setOFPixels) > 1:
-		#image.save('bild' + str(bild) + '.jpeg', 'JPEG')
+	image = np.array(grabImage(gamescreen))
+	box = image[70:120, 180:230]
+	edges = cv2.Canny(box,100,200)
+	#cv2.imwrite('bild' + str(bild) + '.jpeg',box)
+	#bild = bild +1 
+	if np.unique(edges).size > 1:
+		#save(drawRectangle(image), 'bild' + str(bild) + '.jpeg')
 		bild = bild + 1
-		win32api.keybd_event(win32con.VK_SPACE, 0, win32con.KEYEVENTF_EXTENDEDKEY | 0, 0)
-		time.sleep(0.1)
-		win32api.keybd_event(win32con.VK_SPACE, 0, win32con.KEYEVENTF_EXTENDEDKEY | win32con.KEYEVENTF_KEYUP, 0)
+		jump()
                 
